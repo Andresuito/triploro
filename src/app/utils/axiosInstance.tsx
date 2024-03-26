ecc2f4f30e715d911cc85c43e234dc3bb0746f58
Andresuito <barrancorios@gmail.com>
2024-03-26 22:35:10 +0100

    - Testing API online.

--- src/app/utils/axiosInstance.tsx
+++ src/app/utils/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "http://93.93.117.239//api/v1",
   withCredentials: true,
 });
 
