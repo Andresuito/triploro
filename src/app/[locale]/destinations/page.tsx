f6e29fbafdb647e7eabef1d6316acc3dd28ea877
Andresuito <barrancorios@gmail.com>
2024-04-01 23:57:09 +0200

    - Add Map component and integrate it into DestinationsPage

--- src/app/[locale]/destinations/page.tsx
+++ src/app/[locale]/destinations/page.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import Map from "../../components/Map";
 
 export default function DestinationsPage() {
-  return <div className="min-h-screen"></div>;
+  return (
+    <div className="min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="flex justify-between items-center mt-8 md:mt-16">
+          <div>
+            <h2 className="text-lg lg:text-2xl font-semibold text-gray-900">
+              Destinos ofrecidos en{" "}
+              <span className="bg-sky-900 px-1 text-white rounded-md">
+                Triploro
+              </span>
+            </h2>
+          </div>
+        </div>
+        <div className="relative aspect-w-16 aspect-h-9">
+          <Map />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/components/Map.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import React from "react";
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+
+const colorSpainPortugal = "#1f2937";
+const colorEuropeDestinations = "#536d93";
+const colorOtherCountries = "#becada";
+
+const WorldMap = () => {
+  return (
+    <div className="relative aspect-w-16 aspect-h-9">
+      <ComposableMap viewBox="0 30 800 600">
+        <Geographies geography="https://unpkg.com/world-atlas@2.0.2/countries-110m.json">
+          {({ geographies }) =>
+            geographies
+              .filter((geo) => geo.properties.name !== "Antarctica")
+              .map((geo) => {
+                const countryName = geo.properties.name;
+                let color = colorOtherCountries;
+
+                if (["Spain", "Portugal"].includes(countryName)) {
+                  color = colorSpainPortugal;
+                } else if (["Italy", "Germany"].includes(countryName)) {
+                  color = colorEuropeDestinations;
+                }
+
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill={color}
+                    stroke="#FFF"
+                    strokeWidth={0.2}
+                    style={{
+                      default: { pointerEvents: "none" },
+                      hover: { pointerEvents: "none" },
+                      pressed: { pointerEvents: "none" },
+                    }}
+                  />
+                );
+              })
+          }
+        </Geographies>
+      </ComposableMap>
+      <div className="absolute w-full sm:w-fit bottom-1 left-0 bg-opacity-80 rounded-lg p-4 shadow-md text-sm sm:bottom-4 sm:left-5 lg:bottom-60 lg:left-2">
+        <div className="flex justify-between sm:justify-normal sm:flex-col">
+          <div className="flex items-center mb-1">
+            <div className="w-4 h-4 rounded-md bg-sky-900 mr-2"></div>
+            <p>Destinos actuales</p>
+          </div>
+          <div className="flex items-center mb-1">
+            <div className="w-4 h-4 rounded-md bg-[#536d93] mr-2"></div>
+            <p>Futuros destinos</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WorldMap;
