4ae05adaa087ee714954b0d868e8b015b0e94129
Andresuito <barrancorios@gmail.com>
2024-05-04 16:11:20 +0200

    refactor: Update popular destinations grid layout

--- src/app/components/Grid.tsx
+++ src/app/components/Grid.tsx
@@ -55,7 +55,7 @@ export default function DestinosPopulares() {
       <h2 className="text-2xl font-semibold mb-8 mt-5">
         {t("Titles.PopularDestinations")}
       </h2>
-      <div className="grid justify-center md:justify-center lg:justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid justify-center md:justify-center lg:justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 popular-countries">
         {destinos.map((destino, index) => (
           <div key={index} className="relative group">
             <Image
