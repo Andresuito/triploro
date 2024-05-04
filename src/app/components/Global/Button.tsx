93ba44e0598717f7ba2fa179eb77106d100e197a
Andresuito <barrancorios@gmail.com>
2024-05-04 16:12:22 +0200

    refactor: Remove redundant CSS class in Button component

--- src/app/components/Global/Button.tsx
+++ src/app/components/Global/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 interface ButtonProps {
   label: string;
-  onClick: (event: React.FormEvent<HTMLButtonElement>) => void;
+  onClick?: (event: React.FormEvent<HTMLButtonElement>) => void;
   className?: string;
   children?: React.ReactNode;
 }
@@ -12,7 +12,7 @@ const Button: React.FC<ButtonProps> = ({ label, onClick, className }) => {
     <button
       type="submit"
       onClick={onClick}
-      className={`bg-blue text-white w-full py-2 rounded-1xl focus:outline-none transition duration-300 ${className}`}
+      className={`bg-blue text-white py-2 rounded-1xl focus:outline-none transition duration-300 ${className}`}
     >
       {label}
     </button>
