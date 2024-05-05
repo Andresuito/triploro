65652f21b9001e5c13d28f617c4f85b9c03c52e3
Andresuito <barrancorios@gmail.com>
2024-05-05 20:07:29 +0200

    refactor: Update NewItinerary component with date selection and validation

--- src/app/components/New/NewItinerary.tsx
+++ src/app/components/New/NewItinerary.tsx
@@ -1,24 +1,47 @@
 "use client";
 
 import React, { useState, FormEvent } from "react";
+import { useSession } from "next-auth/react";
+import { useTranslations } from "next-intl";
+import axiosInstance from "@/app/utils/axiosInstance";
+
 import Input from "@/app/components/Global/Input";
 import Button from "@/app/components/Global/Button";
-// import Calendar from "@/app/components/Global/Calendar";
-import axiosInstance from "@/app/utils/axiosInstance";
-import { useSession } from "next-auth/react";
+import Calendar from "@/app/components/Global/Calendar";
 
 const NewItinerary = () => {
+  const t = useTranslations("New");
   const { data: session } = useSession();
   const [name, setName] = useState("");
   const [error, setError] = useState("");
   const [succes, setSuccess] = useState("");
 
+  const [days, setDays] = useState(0);
+
   const [highlightEmptyFields, setHighlightEmptyFields] = useState(false);
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+
+  const handleDateSelect = (date: Date) => {
+    if (date < new Date()) {
+      setError("invalid_date");
+      return;
+    }
+
+    setSelectedDate(date);
+    setIsCalendarOpen(false);
+  };
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
 
     if (!name) {
+      setHighlightEmptyFields(true);
+      setError("missing_fields");
+      return;
+    }
+
+    if (!days) {
       setHighlightEmptyFields(true);
       setError("missing_fields");
       return;
@@ -48,36 +71,51 @@ const NewItinerary = () => {
 
   return (
     <div className="mt-8 ml-12">
-      <h1 className="text-blue text-3xl font-semibold">1. Plan a new trip</h1>
+      <h1 className="text-blue text-3xl font-semibold">{t("Form.Title")}</h1>
       <p className="mt-8 mb-7">
-        Choose the destination and dates for your itinerary.
-        <br />
-        Invite your companions, and decide whether to keep it private or share
-        it with the world.
+        {t("Form.Info")} <br />
+        {t("Form.Info2")}
       </p>
       <form onSubmit={handleSubmit} className="w-full ">
         <div className="mb-4">
           <Input
-            label="Where do you want to go?"
+            label={t("Form.Fields.Destination")}
             type="text"
             value={name}
-            placeholder="Location (e.g. Madrid, Lisbon, Kilig)"
+            placeholder={t("Form.Placeholder.Destination")}
             highlightEmpty={highlightEmptyFields}
             hasError={!!error}
             onChange={(value) => setName(value)}
             className="w-full"
           />
-          <div className="flex">
+          <div className="flex space-x-5">
             <Input
-              label="No. of days"
+              label={t("Form.Fields.Days")}
               type="number"
-              value={name}
-              placeholder="# of days"
+              value={days.toString()}
+              placeholder={t("Form.Placeholder.Days")}
               highlightEmpty={highlightEmptyFields}
               hasError={!!error}
-              onChange={(value) => setName(value)}
-              className="w-full"
+              onChange={(value) => setDays(Number(value))}
+              className="w-20"
             />
+            <Input
+              label={t("Form.Fields.TravelDates")}
+              type="text"
+              placeholder={t("Form.Placeholder.TravelDates")}
+              readOnly
+              value={selectedDate ? selectedDate.toLocaleDateString() : ""}
+              onClick={() => setIsCalendarOpen(true)}
+              onChange={() => {}}
+              highlightEmpty={false}
+              hasError={false}
+            />
+            {isCalendarOpen && (
+              <Calendar
+                selectedDate={selectedDate}
+                onDateSelect={handleDateSelect}
+              />
+            )}
           </div>
         </div>
         {succes && (
