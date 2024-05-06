45cbe5eee0539f2b3add73cc29cde93e55627032
Andresuito <barrancorios@gmail.com>
2024-05-06 14:09:09 +0200

    refactor: Update Calendar component with date selection and validation

--- src/app/components/Global/Calendar.tsx
+++ src/app/components/Global/Calendar.tsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, {
+  useState,
+  useMemo,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import {
   MdOutlineKeyboardArrowRight,
   MdOutlineKeyboardArrowLeft,
 } from "react-icons/md";
 import { useTranslations } from "next-intl";
+
+interface CalendarProps {
+  selectedDate: [Date | null, Date | null];
+  onDateSelect: (date: Date, isStartDate: boolean) => void;
+  isCalendarOpen: boolean;
+  isRange: boolean;
+}
 
 const months = [
   "Jan",
@@ -22,25 +35,31 @@ const months = [
 
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-interface CalendarProps {
-  selectedDate: Date | null;
-  onDateSelect: (date: Date) => void;
-}
-
-const Calendar: React.FC<CalendarProps> = ({ onDateSelect, selectedDate }) => {
+const Calendar: React.FC<CalendarProps> = ({
+  onDateSelect,
+  selectedDate,
+  isCalendarOpen,
+  isRange,
+}) => {
   const t = useTranslations("Components.Calendar");
-  const today = new Date();
-  const [currentDate, setCurrentDate] = useState(today);
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [calendarPosition, setCalendarPosition] = useState("bottom");
+  const [hoveredDate, setHoveredDate] = useState<Date | null>(null);
+  const calendarRef = useRef(null);
 
-  const renderDaysOfWeek = () => {
-    return daysOfWeek.map((day, index) => (
-      <div key={index} className="text-sm font-semibold text-gray-500">
-        {t(`Days.${day}`)}{" "}
-      </div>
-    ));
-  };
+  const handleNextMonth = useCallback(() => {
+    setCurrentDate(
+      (prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1)
+    );
+  }, []);
 
-  const renderDays = () => {
+  const handlePreviousMonth = useCallback(() => {
+    setCurrentDate(
+      (prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1)
+    );
+  }, []);
+
+  const generateDays = () => {
     const daysInMonth = new Date(
       currentDate.getFullYear(),
       currentDate.getMonth() + 1,
@@ -64,39 +83,100 @@ const Calendar: React.FC<CalendarProps> = ({ onDateSelect, selectedDate }) => {
         currentDate.getMonth(),
         i
       );
-      const isSelected =
-        selectedDate && currentDay.getTime() === selectedDate.getTime();
-      days.push(
-        <div
-          key={i}
-          className={`text-sm cursor-pointer py-1 hover:bg-slate-100 rounded-md text-center ${
-            isSelected
-              ? "bg-blue text-white hover:bg-slate-100 hover:text-gray-400"
-              : "text-gray-400"
-          }`}
-          onClick={() => onDateSelect(currentDay)}
-        >
-          {i}
-        </div>
-      );
+      days.push(generateDayElement(currentDay, i));
     }
-    return days;
+    return { daysInMonth, firstDayOfMonth, days };
   };
 
-  const handleNextMonth = () => {
-    setCurrentDate(
-      (prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1)
+  const generateDayElement = (currentDay: Date, i: number) => {
+    const isSelected = isDateSelected(currentDay);
+    const isInRange = isDateInRange(currentDay);
+    const isHovered = isDateHovered(currentDay);
+    return (
+      <div
+        key={i}
+        className={`text-sm cursor-pointer py-1 hover:bg-slate-100 rounded-md text-center ${
+          isSelected || isInRange || isHovered
+            ? "bg-blue text-white hover:bg-slate-100 hover:text-gray-400"
+            : "text-gray"
+        }`}
+        onClick={() => handleDateClick(currentDay)}
+        onMouseEnter={() => setHoveredDate(currentDay)}
+      >
+        {i}
+      </div>
     );
   };
 
-  const handlePreviousMonth = () => {
-    setCurrentDate(
-      (prevDate) => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1)
+  const isDateSelected = (date: Date) => {
+    return (
+      (selectedDate[0] && date.getTime() === selectedDate[0].getTime()) ||
+      (selectedDate[1] && date.getTime() === selectedDate[1].getTime())
     );
   };
+
+  const isDateInRange = (date: Date) => {
+    return (
+      selectedDate[0] &&
+      selectedDate[1] &&
+      date.getTime() > selectedDate[0].getTime() &&
+      date.getTime() < selectedDate[1].getTime()
+    );
+  };
+
+  const isDateHovered = (date: Date) => {
+    return (
+      hoveredDate &&
+      selectedDate[0] &&
+      date.getTime() > selectedDate[0].getTime() &&
+      date.getTime() <= hoveredDate.getTime()
+    );
+  };
+
+  const handleDateClick = (date: Date) => {
+    const isStartDate = !selectedDate[0] || !!selectedDate[1];
+    onDateSelect(date, isStartDate);
+  };
+
+  const adjustCalendarPosition = () => {
+    if (isCalendarOpen) {
+      const timer = setTimeout(() => {
+        if (calendarRef.current) {
+          const rect = (
+            calendarRef.current as HTMLElement
+          ).getBoundingClientRect();
+          const spaceBelow = window.innerHeight - rect.bottom;
+          const spaceAbove = rect.top;
+          const enoughSpaceAbove = spaceAbove > rect.height;
+          const enoughSpaceBelow = spaceBelow > rect.height;
+          const nearTopEdge = rect.top < 20;
+
+          if (enoughSpaceBelow || (nearTopEdge && !enoughSpaceAbove)) {
+            setCalendarPosition("top-20");
+          } else {
+            setCalendarPosition("bottom-16");
+          }
+        }
+      }, 0);
+
+      return () => clearTimeout(timer);
+    }
+  };
+
+  const { days } = useMemo(
+    () => generateDays(),
+    [currentDate, selectedDate, onDateSelect, hoveredDate]
+  );
+
+  useEffect(() => {
+    adjustCalendarPosition();
+  }, [isCalendarOpen]);
 
   return (
-    <div className="p-5 bg-white rounded shadow-md ring-1 ring-black ring-opacity-5 select-none">
+    <div
+      ref={calendarRef}
+      className={`absolute ${calendarPosition}  p-5 bg-white rounded shadow-md ring-1 ring-black ring-opacity-5 select-none`}
+    >
       <div className="flex justify-between items-center mb-4">
         <div
           onClick={handlePreviousMonth}
@@ -116,8 +196,12 @@ const Calendar: React.FC<CalendarProps> = ({ onDateSelect, selectedDate }) => {
         </div>
       </div>
       <div className="grid grid-cols-7 gap-1">
-        {renderDaysOfWeek()}
-        {renderDays()}
+        {daysOfWeek.map((day, index) => (
+          <div key={index} className="text-sm font-semibold text-gray-500">
+            {t(`Days.${day}`)}{" "}
+          </div>
+        ))}
+        {days}
       </div>
     </div>
   );
