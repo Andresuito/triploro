9d9daf85d7c9f59d860ba7417be73dc80f11e032
Andresuito <barrancorios@gmail.com>
2024-05-07 23:56:46 +0200

    refactor: Update Profile component with improved loading skeleton and translations

--- src/app/components/Profile/Skeleton.tsx
+++ src/app/components/Profile/Skeleton.tsx
@@ -1,26 +1,43 @@
 import React from "react";
+import { useTranslations } from "next-intl";
 
 export default function Skeleton() {
+  const t = useTranslations("Profile");
+
   return (
-    <div className="min-h-screen">
-      <div className="max-w-7xl mx-auto px-4 py-8">
-        <div className="flex justify-between items-center mt-8 md:mt-5">
-          <h2 className="text-lg lg:text-2xl font-semibold text-gray-900 mb-3">
-            Hola,{" "}
-            <span className="bg-gray-600 text-white px-16 rounded-lg animate-pulse">
+    <div className="min-h-screen py-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mb-10">
+          <h2 className="text-2xl font-semibold text-gray-900 mb-3">
+            {t("Greeting")},{" "}
+            <span className="bg-gray-600 text-white px-16 py-1 rounded-lg animate-pulse">
               &nbsp;
             </span>
           </h2>
         </div>
-        <div>
+        <div className="mb-10">
           <h2 className="text-lg lg:text-2xl font-semibold text-gray-900 mb-3">
-            Destinos favoritos
+            {t("YourTrips")}
           </h2>
+          <span className="bg-gray-600 text-white px-40 py-1 rounded-lg animate-pulse">
+            &nbsp;
+          </span>
         </div>
-        <div>
+        <div className="mb-10">
           <h2 className="text-lg lg:text-2xl font-semibold text-gray-900 mb-3">
-            Itinerarios favoritos
+            {t("FavoriteDestinations")}
           </h2>
+          <span className="bg-gray-600 text-white px-40 py-1 rounded-lg animate-pulse">
+            &nbsp;
+          </span>
+        </div>
+        <div className="mb-10">
+          <h2 className="text-lg lg:text-2xl font-semibold text-gray-900 mb-3">
+            {t("FavoriteItineraries")}
+          </h2>
+          <span className="bg-gray-600 text-white px-40 py-1 rounded-lg animate-pulse">
+            &nbsp;
+          </span>
         </div>
       </div>
     </div>
