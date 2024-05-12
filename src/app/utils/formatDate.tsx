693e2c7480fe1a76181518b3bd3e7a3b138edeb5
Andresuito <barrancorios@gmail.com>
2024-05-12 16:21:17 +0200

    refactor: Add formatRangeDate function for formatting date ranges

--- src/app/utils/formatDate.tsx
+++ src/app/utils/formatDate.tsx
@@ -11,3 +11,19 @@ export const formatDate = (dateString: any) => {
   parts[1] = parts[1].charAt(0).toUpperCase() + parts[1].slice(1);
   return parts.join(" ");
 };
+
+export const formatRangeDate = (startDate: any, endDate: any) => {
+  const options: Intl.DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+  };
+
+  const start = new Date(startDate)
+    .toLocaleDateString("en-US", options)
+    .toUpperCase();
+  const end = new Date(endDate)
+    .toLocaleDateString("en-US", options)
+    .split(" ")[1];
+
+  return `${start}-${end}`;
+};
