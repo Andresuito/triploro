096b32f46beeb64a2e47184fa649ca335cd65828
Andresuito <barrancorios@gmail.com>
2024-05-14 21:16:34 +0200

    feat: Add lazy loading for images in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "triploro.es",
+        port: "",
+        pathname: "/uploads/",
+      },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
