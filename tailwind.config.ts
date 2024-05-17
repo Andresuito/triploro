e9fceed80ec91985a9e71b17664bd306bda80624
Andresuito <barrancorios@gmail.com>
2024-05-17 17:30:27 +0200

    feat: Add xs screen size to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: "375px",
+      },
       colors: {
         blue: "hsl(var(--primary-color) / <alpha-value>)"
       },
