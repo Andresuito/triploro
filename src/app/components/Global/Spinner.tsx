2f6e2baa791976850228af4267a20688a5c5d104
Andresuito <barrancorios@gmail.com>
2024-05-20 19:37:06 +0200

    refactor: Add Spinner component for displaying loading state

---
+++ src/app/components/Global/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="h-auto flex items-center justify-center">
+      <div className="animate-spin rounded-full h-20 w-20 border-t-4 border-blue"></div>
+    </div>
+  );
+};
+
+export default Spinner;
