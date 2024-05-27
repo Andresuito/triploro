45817c06d908c587454d043a723e698285f8d890
Andresuito <barrancorios@gmail.com>
2024-05-27 13:51:43 +0200

    refactor: Update TripsUser component to use SafeImage for lazy loading and error handling of trip images

--- src/app/[locale]/itinerary/[code]/page.tsx
+++ src/app/[locale]/itinerary/[code]/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import axiosInstance from "@/app/utils/axiosInstance";
 import { useSession } from "next-auth/react";
 import { IoCloudUploadOutline } from "react-icons/io5";
@@ -10,16 +10,19 @@ import Toast from "@/app/components/Global/Toast";
 import type { Itinerary } from "@/types/itineraryType";
 import { formatRangeDate } from "@/app/utils/formatDate";
 import { useRouter } from "next/navigation";
+import SkeletonItinerary from "@/app/components/Itinerary/Code/Skeleton";
 
-import Spinner from "@/app/components/Global/Spinner";
 import SafeImage from "@/app/components/SafeImage";
 import NotImage from "@/app/assets/pattern.svg";
+import { useTranslations } from "next-intl";
 
 type Props = {
   params: { code: string; locale: string };
 };
 
 export default function Itinerary({ params }: Props) {
+  const t = useTranslations("Itinerary");
+  const c = useTranslations("Country.Cities");
   const router = useRouter();
   const { data: session } = useSession();
   const [itinerary, setItinerary] = useState<Itinerary | null>(null);
@@ -99,7 +102,11 @@ export default function Itinerary({ params }: Props) {
 
         if (response.status === 200) {
           Toast({ message: "Image uploaded correctly.", isError: false });
-          setPreviewUrl(URL.createObjectURL(image));
+          const newImageUrl = URL.createObjectURL(image);
+          setPreviewUrl(newImageUrl);
+          setItinerary((prevState) =>
+            prevState ? { ...prevState, imageUrl: newImageUrl } : null
+          );
           setImage(null);
           setShowUpload(false);
         } else {
@@ -135,7 +142,6 @@ export default function Itinerary({ params }: Props) {
           type: file.type,
         });
         setImage(file);
-        await handleUpload();
       } else {
         Toast({
           message: "Please upload a PNG or JPEG image of size less than 1MB",
@@ -161,8 +167,6 @@ export default function Itinerary({ params }: Props) {
           type: file.type,
         });
         setImage(file);
-        setPreviewUrl(URL.createObjectURL(file));
-        await handleUpload();
       } else {
         Toast({
           message: "Please upload a PNG or JPEG image of size less than 1MB",
@@ -173,109 +177,114 @@ export default function Itinerary({ params }: Props) {
   };
 
   return (
-    <>
+    <div className="min-h-screen max-w-7xl mx-auto mt-8 md:mt-16">
       {isLoading ? (
-        <Spinner />
+        <SkeletonItinerary />
       ) : (
         itinerary && (
-          <div className="min-h-screen">
-            <div className="max-w-7xl mx-auto mt-8 md:mt-16">
-              <div className="mt-10 w-96">
-                <div className="flex justify-center relative">
-                  <SafeImage
-                    src={
-                      previewUrl
-                        ? previewUrl
-                        : itinerary.imageUrl
-                        ? `https://triploro.es${
-                            itinerary.imageUrl
-                          }?${new Date().getTime()}`
-                        : NotImage
-                    }
-                    alt={itinerary.city ? itinerary.city : ""}
-                    width={400}
-                    height={300}
-                    className="w-96 h-[300px] object-cover rounded-t-1xl opacity-90"
-                  />
+          <div className="flex">
+            <div className=" w-96">
+              <div className="flex justify-center relative">
+                <SafeImage
+                  src={
+                    previewUrl
+                      ? previewUrl
+                      : itinerary.imageUrl
+                      ? `http://localhost:8000${itinerary.imageUrl}`
+                      : NotImage
+                  }
+                  alt={itinerary.city ? itinerary.city : ""}
+                  width={400}
+                  height={300}
+                  className="w-96 h-[300px] object-cover rounded-t-1xl opacity-90"
+                />
+                {itinerary.isOwner && (
                   <p
                     className="absolute bottom-1 right-1 text-xs text-white hover:bg-blue p-1 cursor-pointer rounded-md"
                     onClick={() => setShowUpload(true)}
                   >
-                    Edit Cover Photo
+                    {t("Buttons.NewImage")}
                   </p>
-                  {showUpload && (
-                    <div className="absolute flex items-center justify-center w-full h-full">
-                      <label
-                        className={`flex flex-col items-center justify-center w-full h-full border-2 ${
-                          image
-                            ? "border-blue hover:border-blue/80 border-solid text-blue"
-                            : "border-blue border-dashed"
-                        } rounded cursor-pointer ${
-                          image ? "bg-white/20 border-0" : "bg-white/70"
-                        } text-blue`}
-                        onDragOver={handleDragOver}
-                        onDrop={handleDrop}
-                      >
-                        <IoCloudUploadOutline className="text-2xl mb-1" />
-                        <p className="mb-1 text-xs">
-                          <span className="font-semibold">Click or Drop</span>
-                        </p>
-                        <p className="text-xs">PNG, JPG (MAX. 380x300px)</p>
-                        <input
-                          id="dropzone-file"
-                          type="file"
-                          className="hidden"
-                          onChange={handleFileChange}
-                        />
-                      </label>
-                      <button
-                        className="absolute top-2 right-2 text-white bg-blue/50 hover:bg-blue/100 rounded-full px-1 py-1 text-sm"
-                        onClick={() => setShowUpload(false)}
-                      >
-                        <IoMdClose />
-                      </button>
-                    </div>
-                  )}
+                )}
+                {showUpload && (
+                  <div className="absolute flex items-center justify-center w-full h-full">
+                    <label
+                      className={`flex flex-col items-center justify-center w-full h-full border-2 ${
+                        image
+                          ? "border-blue hover:border-blue/80 border-solid text-blue"
+                          : "border-blue border-dashed"
+                      } rounded cursor-pointer ${
+                        image ? "bg-white/20 border-0" : "bg-white/70"
+                      } text-blue`}
+                      onDragOver={handleDragOver}
+                      onDrop={handleDrop}
+                    >
+                      <IoCloudUploadOutline className="text-2xl mb-1" />
+                      <p className="mb-1 text-xs">
+                        <span className="font-semibold">
+                          {t("Buttons.NewImageDropOrClick")}
+                        </span>
+                      </p>
+                      <p className="text-xs">PNG, JPG (MAX. 380x300px)</p>
+                      <input
+                        id="dropzone-file"
+                        type="file"
+                        className="hidden"
+                        onChange={handleFileChange}
+                      />
+                    </label>
+                    <button
+                      className="absolute top-2 right-2 text-white bg-blue/50 hover:bg-blue/100 rounded-full px-1 py-1 text-sm"
+                      onClick={() => setShowUpload(false)}
+                    >
+                      <IoMdClose />
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="p-5 rounded-b-1xl shadow-2xl">
+                <div className="flex justify-between items-center">
+                  <p className="text-3xl text-blue font-semibold">
+                    {c(itinerary.city) || itinerary.city}
+                  </p>
+                  <p className="p-1 px-1 text-xs bg-blue text-white w-fit rounded-md">
+                    {itinerary.code}
+                  </p>
                 </div>
-                <div className="p-5 rounded-b-1xl shadow-2xl">
-                  <div className="flex justify-between items-center">
-                    <p className="text-3xl text-blue font-semibold">
-                      {itinerary.city}
+                <div className="flex mt-6 space-x-20 text-1xl text-center text-blue">
+                  <div className="flex flex-col">
+                    <p>
+                      {
+                        formatRangeDate(
+                          itinerary.startDate,
+                          itinerary.endDate
+                        ).split(" ")[0]
+                      }
                     </p>
-                    <p className="p-1 px-1 text-xs bg-blue text-white w-fit rounded-md">
-                      {itinerary.code}
+                    <p className="-mt-2">
+                      {
+                        formatRangeDate(
+                          itinerary.startDate,
+                          itinerary.endDate
+                        ).split(" ")[1]
+                      }
                     </p>
                   </div>
-                  <div className="flex mt-6 space-x-20 text-1xl text-center text-blue">
-                    <div className="flex flex-col">
-                      <p>
-                        {
-                          formatRangeDate(
-                            itinerary.startDate,
-                            itinerary.endDate
-                          ).split(" ")[0]
-                        }
-                      </p>
-                      <p className="-mt-2">
-                        {
-                          formatRangeDate(
-                            itinerary.startDate,
-                            itinerary.endDate
-                          ).split(" ")[1]
-                        }
-                      </p>
-                    </div>
-                    <div className="flex flex-col">
-                      <p>{itinerary.days}</p>
-                      <p className="-mt-2">Dias</p>
-                    </div>
+                  <div className="flex flex-col">
+                    <p>{itinerary.days}</p>
+                    <p className="-mt-2">{t("Days")}</p>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="ml-10">
+              <p className="text-3xl text-blue font-semibold">
+                {c(itinerary.city) || itinerary.city}
+              </p>
+            </div>
           </div>
         )
       )}
-    </>
+    </div>
   );
 }
