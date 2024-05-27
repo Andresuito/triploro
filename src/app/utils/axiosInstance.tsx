985cfb059911d50ca28c5776e45bc8e2bfba7fd2
Andresuito <barrancorios@gmail.com>
2024-05-27 13:51:14 +0200

    refactor: Update axiosInstance baseURL to use environment variable

--- src/app/utils/axiosInstance.tsx
+++ src/app/utils/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://triploro.es/api/v1",
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
   withCredentials: true,
 });
 
