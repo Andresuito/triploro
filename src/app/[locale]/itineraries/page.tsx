bc00f26f01952b784f619e85a7fa9a836046523e
Andresuito <barrancorios@gmail.com>
2024-05-27 13:51:38 +0200

    refactor: Update TripsUser component to use SafeImage for lazy loading and error handling of trip images

--- src/app/[locale]/itineraries/page.tsx
+++ src/app/[locale]/itineraries/page.tsx
@@ -1,3 +1,5 @@
+// ItinerariesPublics.tsx
+
 "use client";
 
 import React, { useEffect, useState } from "react";
@@ -13,15 +15,24 @@ import NotImage from "@/app/assets/pattern.svg";
 
 import Button from "@/app/components/Global/Button";
 import Spinner from "@/app/components/Global/Spinner";
+import { useTranslations } from "next-intl";
 
 export default function ItinerariesPublics() {
   const [itineraries, setItineraries] = React.useState<Itinerary[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [loadedItineraries, setLoadedItineraries] = useState(20);
+  const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const t = useTranslations("Itineraries");
+  const c = useTranslations("Country.Cities");
 
   const getItineraries = async () => {
     setIsLoading(true);
     try {
-      const response = await axiosInstance.get("/itinerary/allPublic");
+      const response = await axiosInstance.get("/itinerary/allPublic", {
+        params: {
+          limit: loadedItineraries,
+        },
+      });
       if (response.status === 200 && Array.isArray(response.data)) {
         const modifiedData = response.data.map((item) => ({
           ...item,
@@ -37,7 +48,48 @@ export default function ItinerariesPublics() {
 
   useEffect(() => {
     getItineraries();
-  }, []);
+  }, [loadedItineraries]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollTop = window.scrollY || document.documentElement.scrollTop;
+      const clientHeight = document.documentElement.clientHeight;
+      if (scrollTop + clientHeight >= scrollHeight - 200) {
+        loadMoreItineraries();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [loadedItineraries]);
+
+  const loadMoreItineraries = async () => {
+    if (!isLoadingMore) {
+      setIsLoadingMore(true);
+      try {
+        const response = await axiosInstance.get("/itinerary/allPublic", {
+          params: {
+            limit: 20,
+            offset: loadedItineraries,
+          },
+        });
+        if (response.status === 200 && Array.isArray(response.data)) {
+          const newData = response.data.map((item) => ({
+            ...item,
+            height: `${Math.floor(Math.random() * 200) + 200}px`,
+          }));
+          setItineraries((prevData) => [...prevData, ...newData]);
+          setLoadedItineraries((prevCount) => prevCount + 20);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      setIsLoadingMore(false);
+    }
+  };
 
   const breakpointColumnsObj = {
     default: 4,
@@ -48,7 +100,7 @@ export default function ItinerariesPublics() {
 
   const itineraryImages = itineraries.map((itinerary) => {
     if (itinerary.imageUrl) {
-      return `https://triploro.es${itinerary.imageUrl}`;
+      return `${process.env.NEXT_PUBLIC_API_URL}${itinerary.imageUrl}`;
     } else {
       return null;
     }
@@ -59,9 +111,9 @@ export default function ItinerariesPublics() {
       <div className="max-w-7xl mx-auto px-4 py-8">
         <div className="mt-8 md:mt-16 flex justify-between items-center mb-8">
           <h2 className="text-base lg:text-2xl font-semibold text-gray-900 ">
-            Todos nuestros{" "}
+            {t("Title")}
             <span className="bg-blue px-2 py-2 text-white rounded-1xl">
-              Itinerarios
+              {t("TitleMore")}
             </span>
           </h2>
 
@@ -69,58 +121,55 @@ export default function ItinerariesPublics() {
             href="/new"
             className="text-sm lg:text-base underline underline-offset-4 hover:bg-blue align-top px-2 py-2 hover:no-underline rounded-1xl duration-300 transition hover:text-white"
           >
-            Crear Itinerario
+            {t("Button.Create")}
           </Link>
         </div>
-        {isLoading ? (
-          <Spinner />
-        ) : (
+        {itineraries.length === 0 && <Spinner />}
+        {itineraries.length > 0 && (
           <Masonry
             breakpointCols={breakpointColumnsObj}
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column"
           >
-            {itineraries.map((itinerary: Itinerary, index: number) => {
-              return (
-                <Link
-                  href={`/itinerary/${itinerary.code}`}
-                  key={itinerary.code}
-                  legacyBehavior
-                  className="pointer-events-none"
+            {itineraries.map((itinerary: Itinerary, index: number) => (
+              <Link
+                href={`/itinerary/${itinerary.code}`}
+                key={itinerary.code}
+                legacyBehavior
+                className="pointer-events-none"
+              >
+                <div
+                  key={itinerary.id}
+                  className="group rounded-1xl text-white p-4 text-center mb-4 overflow-hidden relative "
+                  style={{
+                    height: itinerary.height,
+                  }}
                 >
-                  <div
-                    key={itinerary.id}
-                    className="group rounded-1xl text-white p-4 text-center mb-4 overflow-hidden relative "
-                    style={{
-                      height: itinerary.height,
-                    }}
-                  >
-                    <SafeImage
-                      src={
-                        itineraryImages[index]
-                          ? `${itineraryImages[index]}`
-                          : NotImage.src
-                      }
-                      alt={itinerary.city}
-                      fill
-                      quality={70}
-                      className="cursor-pointer opacity-75 rounded-1xl hover:opacity-100 transition-opacity duration-300 object-cover"
-                    />
-                    <div className="absolute text-white top-2 right-5 md:right-5">
-                      <FavoriteItinerary code={itinerary.code} />
-                    </div>
-                    <div className="absolute top-2 left-2 bg-white rounded-full text-blue font-bold px-2 text-sm opacity-100 md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 select-none">
-                      {itinerary.days} Dias
-                    </div>
-                    <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-80 py-1 md:py-2 text-blue text-center opacity-100 md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 select-none">
-                      <h3 className="text-base font-semibold mx-2">
-                        {itinerary.city}
-                      </h3>
-                    </div>
+                  <SafeImage
+                    src={
+                      itineraryImages[index]
+                        ? `${itineraryImages[index]}`
+                        : NotImage.src
+                    }
+                    alt={itinerary.city}
+                    fill
+                    quality={70}
+                    className="cursor-pointer opacity-75 rounded-1xl hover:opacity-100 transition-opacity duration-300 object-cover"
+                  />
+                  <div className="absolute text-white top-2 right-5 md:right-5">
+                    <FavoriteItinerary code={itinerary.code} />
                   </div>
-                </Link>
-              );
-            })}
+                  <div className="absolute top-2 left-2 bg-white rounded-full text-blue font-bold px-2 text-sm opacity-100 md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 select-none">
+                    {itinerary.days} {t("TripInfo.Days")}
+                  </div>
+                  <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-80 py-1 md:py-2 text-blue text-center opacity-100 md:opacity-0 transition-opacity duration-300 md:group-hover:opacity-100 select-none">
+                    <h3 className="text-base font-semibold mx-2">
+                      {c(itinerary.city) || itinerary.city}
+                    </h3>
+                  </div>
+                </div>
+              </Link>
+            ))}
           </Masonry>
         )}
       </div>
