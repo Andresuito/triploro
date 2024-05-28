8c65427de717dc986f1836df0e088a84c931ee69
Andresuito <barrancorios@gmail.com>
2024-05-28 14:23:47 +0200

    refactor: Improve VerifyEmail page by removing unused code, fixing redirect issue, and adding error messages

--- src/app/components/Auth/VerifyEmail.tsx
+++ src/app/components/Auth/VerifyEmail.tsx
@@ -1,26 +1,31 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { getSession } from "next-auth/react";
 import axiosInstance from "@/app/utils/axiosInstance";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 const VerifyEmail = () => {
+  const t = useTranslations("VerifyEmail");
   const searchParams = useSearchParams();
   const token = searchParams.get("token");
-  const router = useRouter();
   const [error, setError] = useState<string | null>(null);
+  const [message, setMessage] = useState<string | null>(null);
+  const [status, setStatus] = useState<string | null>(null);
 
   useEffect(() => {
     const checkSession = async () => {
       const session = await getSession();
       if (session) {
-        router.push("/");
+        setStatus(t("Errors.Error"));
+        setMessage(t("Errors.AlreadyVerifed"));
       } else if (token) {
         sendVerificationEmail(token);
       } else {
-        router.push("/");
+        setStatus(t("Error"));
+        setMessage(t("Errors.NoToken"));
       }
     };
 
@@ -41,29 +46,36 @@ const VerifyEmail = () => {
         );
 
         if (response.status === 200) {
-          router.push("/");
+          setStatus(t("Success.Welcome"));
+          setMessage(t("Success.Message"));
         }
       } catch (error: any) {
         if (error.response.data.error === "token_expired") {
-          setError("Token expired");
-        } else if (error.response.data.error === "invalid_token") {
-          setError("Invalid token");
+          setStatus(error.response.status);
+          setError(t("Errors.TokenExpired"));
+        } else if (
+          error.response.data.error === "invalid_token" ||
+          error.response.status === 400
+        ) {
+          setStatus(error.response.status);
+          setError(t("Errors.InvalidToken"));
         }
+        setStatus(error.response.status);
       }
     };
-  }, [token, router]);
+  }, [token]);
 
-  if (error) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen text-sky-900">
-        <h1 className="text-9xl font-bold mb-4 text-sky-900">400</h1>
-        <p className="text-2xl mb-4">{error}</p>
-        <Link href="/" legacyBehavior>
-          <a className="text-sky-800 text-lg hover:text-sky-950"></a>
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-sky-900">
+      <h1 className="text-6xl font-bold mb-4 bg-blue p-5 text-white rounded">
+        {status}
+      </h1>
+      <p className="text-2xl mb-4 text-center">{error || message}</p>
+      <Link href="/" legacyBehavior>
+        <a className="text-sky-800 text-lg hover:text-sky-950">{t("Back")}</a>
+      </Link>
+    </div>
+  );
 };
 
 export default VerifyEmail;
