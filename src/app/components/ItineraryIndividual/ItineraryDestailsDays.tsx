d284d91d91e8e08ac1ee9ff919991d996fad8439
Andresuito <barrancorios@gmail.com>
2024-05-30 21:24:18 +0200

    refactor: Update ItineraryDetailsDays component to use lazy loading for images

--- src/app/components/ItineraryIndividual/ItineraryDestailsDays.tsx
+++ src/app/components/ItineraryIndividual/ItineraryDestailsDays.tsx
@@ -1,21 +1,30 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-
-import { useState, useEffect } from "react";
-import { useSpring, animated } from "react-spring";
+import React, { useState, useEffect } from "react";
+import { OptionActivity } from "./OptionActivity";
 
 interface Day {
   day: string;
   date: string;
   description: string;
+  activityInfo: JSX.Element[];
 }
 
-const days: Day[] = [
-  { day: "Día 1", date: "Enero 1, 2024", description: "Descripción del día 1" },
-  { day: "Día 2", date: "Enero 2, 2024", description: "Descripción del día 2" },
-  { day: "Día 3", date: "Enero 3, 2024", description: "Descripción del día 3" },
-];
+interface ItineraryDetailsDaysProps {
+  itinerary: any;
+}
 
-export const ItineraryDetailsDays: React.FC = () => {
+export const ItineraryDetailsDays: React.FC<ItineraryDetailsDaysProps> = ({
+  itinerary,
+}) => {
+  const generateInitialDays = (num: number): Day[] => {
+    return Array.from({ length: num }, (_, i) => ({
+      day: `Día ${i + 1}`,
+      date: "",
+      description: "",
+      activityInfo: [],
+    }));
+  };
+
+  const [days, setDays] = useState<Day[]>(generateInitialDays(itinerary.days));
   const [openDays, setOpenDays] = useState<{ [key: number]: boolean }>({});
   const [closingDays, setClosingDays] = useState<{ [key: number]: boolean }>(
     {}
@@ -33,44 +42,62 @@ export const ItineraryDetailsDays: React.FC = () => {
     return () => clearTimeout(timer);
   }, [openDays]);
 
-  return (
-    <ol className="relative border-l border-gray-200 text-blue mt-10">
-      {days.map((item, index) => {
-        const isOpen =
-          openDays[index as keyof typeof openDays] ||
-          closingDays[index as keyof typeof closingDays];
-        const animation = useSpring({
-          opacity: isOpen ? 1 : 0,
-          transform: isOpen ? "translateX(0)" : "translateX(-20px)",
-          config: { mass: 1, tension: 280, friction: 120 },
-        });
+  const addActivityToDay = (activityInfo: JSX.Element, dayIndex: number) => {
+    const updatedDays = [...days];
+    updatedDays[dayIndex].activityInfo.push(
+      React.cloneElement(activityInfo, {
+        key: `${dayIndex}-${updatedDays[dayIndex].activityInfo.length}`,
+      })
+    );
+    setDays(updatedDays);
+  };
 
-        return (
-          <li key={index} className="mb-10 ml-6">
-            <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white">
-              <span className="text-white w-10 bg-blue rounded-full flex items-center justify-center">
-                {index + 1}
+  return (
+    <div className="mt-10">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-semibold text-blue mb-5">
+          Información del Itinerario
+        </h1>
+      </div>
+      <ol className="relative border-l border-gray-200 text-blue">
+        {days.map((item, index) => {
+          const isOpen =
+            openDays[index as keyof typeof openDays] ||
+            closingDays[index as keyof typeof closingDays];
+
+          return (
+            <li key={index} className={`mb-10 ml-6`}>
+              <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -left-3 ring-8 ring-white">
+                <span className="text-white w-10 bg-blue rounded-full flex items-center justify-center">
+                  {index + 1}
+                </span>
               </span>
-            </span>
-            <h3
-              className="flex items-center mb-1 text-base font-semibold bg-blue w-fit text-white px-5 rounded cursor-pointer"
-              onClick={() => toggleDay(index)}
-            >
-              {item.day}
-            </h3>
-            {isOpen && (
-              <animated.div style={animation}>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400">
-                  {item.date}
-                </time>
-                <p className="text-base font-normal text-gray-500 border border-blue rounded p-2">
-                  {item.description}
-                </p>
-              </animated.div>
-            )}
-          </li>
-        );
-      })}
-    </ol>
+              <h3
+                className="flex items-center mb-1 text-base font-semibold bg-blue w-fit text-white px-5 rounded cursor-pointer"
+                onClick={() => toggleDay(index)}
+              >
+                {item.day}
+              </h3>
+              {isOpen && (
+                <div>
+                  <div className="flex justify-between items-center mb-2">
+                    <time className="block text-sm font-normal leading-none text-gray-400">
+                      {item.date}
+                    </time>
+                  </div>
+                  <OptionActivity
+                    isOwner={itinerary.isOwner}
+                    key={index}
+                    onActivityAdd={(activityInfo) =>
+                      addActivityToDay(activityInfo, index)
+                    }
+                  />
+                </div>
+              )}
+            </li>
+          );
+        })}
+      </ol>
+    </div>
   );
 };
