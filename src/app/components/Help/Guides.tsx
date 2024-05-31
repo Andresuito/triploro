959ea791df1f255e75e2769fd59f3552a861e800
Andresuito <barrancorios@gmail.com>
2024-05-31 12:48:12 +0200

    refactor: Update help page to use lazy loading for images

--- src/app/components/Help/Guides.tsx
+++ src/app/components/Help/Guides.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import { useTranslations } from "next-intl";
-import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import Image from "next/image";
-
 import help1 from "@/app/assets/help-1.jpg";
 import help2 from "@/app/assets/help-2.jpg";
 import help3 from "@/app/assets/help-3.jpg";
@@ -13,21 +11,25 @@ const Guides = () => {
 
   const helpItems = [
     {
+      slug: "how-make-your-trip",
       image: help1,
       alt: t("Topics.Help1"),
       title: t("Topics.Help1"),
     },
     {
+      slug: "itineraries-in-triploro",
       image: help2,
       alt: t("Topics.Help2"),
       title: t("Topics.Help2"),
     },
     {
+      slug: "share-your-experiences",
       image: help3,
       alt: t("Topics.Help3"),
       title: t("Topics.Help3"),
     },
     {
+      slug: "how-travel-with-friends",
       image: help4,
       alt: t("Topics.Help4"),
       title: t("Topics.Help4"),
@@ -43,30 +45,23 @@ const Guides = () => {
             <span className="bg-blue px-1 text-white rounded-md">Triploro</span>
           </h2>
         </div>
-        <div className="hidden md:flex items-center">
-          <Link legacyBehavior href="/all-articles">
-            <a className="hover:text-blue duration-200 transition">
-              {t("Articles")}
-              <MdOutlineKeyboardArrowRight
-                className="inline-block ml-1"
-                size={20}
-              />
-            </a>
-          </Link>
-        </div>
       </div>
       <div className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         {helpItems.map((item, index) => (
           <div key={index}>
-            <Image
-              src={item.image}
-              alt={item.alt}
-              placeholder="blur"
-              className="rounded-md w-full h-60 object-cover object-top shadow-xl"
-            />
-            <h1 className="mt-2 cursor-pointer hover:text-blue duration-200 transition">
-              {item.title}
-            </h1>
+            <Link href={`/help/${item.slug}`} legacyBehavior>
+              <a>
+                <Image
+                  src={item.image}
+                  alt={item.alt}
+                  placeholder="blur"
+                  className="rounded-md w-full h-60 object-cover object-top shadow-xl"
+                />
+                <h1 className="mt-2 cursor-pointer hover:text-blue duration-200 transition">
+                  {item.title}
+                </h1>
+              </a>
+            </Link>
           </div>
         ))}
       </div>
